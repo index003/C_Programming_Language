968ad1f614700d8004d3679eec3e9aec75c15a80
index003 <index003@sina.cn>
2021-12-17 11:21:04 +0800

    update 1-12

--- chapter_01/1.5.4_practice_1_12.c
+++ chapter_01/1.5.4_practice_1_12.c
@@ -19,5 +19,18 @@ int main() {
             putchar(c);
         }
     }
+    /*
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\t' || c == '\n') {
+            if (state == IN) {
+                putchar('\n');
+                state = OUT;
+            }
+        } else {
+            state = IN;
+            putchar(c);
+        }   //这种方式代码虽然少一些，但是state赋值操作太频繁
+    }
+    */
 }
 
