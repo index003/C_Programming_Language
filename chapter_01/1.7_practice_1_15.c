a76ce5aa4b64314b8873fe731361be07f609a6e5
index003 <index003@sina.cn>
2021-12-17 15:18:45 +0800

    update 1-15

--- chapter_01/1.7_practice_1_15.c
+++ chapter_01/1.7_practice_1_15.c
@@ -1,24 +1,24 @@
 # include <stdio.h>
 
-void fahr_celsius(float lower, float upper,float step); 
+float fahr_celsius(float fahr); 
 
 int main() {
+    float fahr, celsius;
     float lower, upper, step;
     lower = 0;
     upper = 300;
     step = 20;
-    fahr_celsius(lower, upper, step);
+    fahr = lower;
+    while (fahr < upper) {
+       celsius = fahr_celsius(fahr);
+       printf("%5.1f,%8.2f\n", fahr, celsius);
+       fahr = fahr + step;
+    }
 }
 
 
-void fahr_celsius(float lower, float upper,float step) {
-    float fahr, celsius;
-
-    fahr = lower;
-    while (fahr <= upper) {
-        celsius = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
-    }
-
+float fahr_celsius(float fahr) {
+    float celsius;
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    return celsius;
 }
