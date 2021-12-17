894e59d9ea8743bc9ffd1c177badaff559b30050
index003 <index003@sina.cn>
2021-12-17 14:46:38 +0800

    update 1-14

--- chapter_01/1.6_practice_1_14.c
+++ chapter_01/1.6_practice_1_14.c
@@ -1,40 +1,34 @@
 #include <stdio.h>
 
+#define MAX_CHAR 128
+
 int main() {
-    int c, nwhite, nother;
-    int ndigit[10];
-    nwhite = nother = 0;
-    for (int i = 0; i < 10;i++) {
-        ndigit[i] = 0;
+    int c;
+    int cc[MAX_CHAR];
+    for (int i = 0; i < MAX_CHAR;i++) {
+        cc[i] = 0;
     }
 
     while((c = getchar()) != EOF) {
-        if(c == ' ' || c == '\n' || c == '\t') {
-            ++nwhite;
-        } else if (c >= '0' && c <='9') {
-            ++ndigit[c - '0'];
-        } else {
-            ++nother;
-        }
+        ++cc[c];
     }
 
-    for (int i = 0; i < 10; i++) {
-        printf("ndigit[%d]: %d", i, ndigit[i]);
-        for (int j = 0; j < ndigit[i]; j++) {
-            printf("=");
+    for (int j = 0; j < MAX_CHAR; j++) {
+        if (cc[j] > 0) {
+            if (j == ' ') {
+                printf("space");
+            } else if (j == '\n') {
+                printf(" wrap");
+            } else if (j == '\t') {
+                printf("  tab");
+            } else {
+                printf("%5c", j);
+            }
+            printf("  %3d  ", cc[j]);
+            for (int k = 0; k < cc[j]; k++) {
+                printf("=");
+            }
+            printf("\n");
         }
-        printf("\n");
     }
-    
-    printf("nwhite:    %d", nwhite);
-    for (int k = 0; k < nwhite; k++) {
-        printf("=");
-    }
-    printf("\n");
-
-    printf("nother:    %d", nother);
-    for (int l = 0; l < nother; l++) {
-        printf("=");
-    }
-    printf("\n");
 }
