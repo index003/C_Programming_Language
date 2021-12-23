2fe3922af4a627530f024180fe19150ffc9769d4
index003 <index003@sina.cn>
2021-12-23 20:09:40 +0800

    update 18

--- chapter_01/1.9_exercise_1_18.c
+++ chapter_01/1.9_exercise_1_18.c
@@ -1,51 +1,24 @@
 #include <stdio.h>
-#define MAXLINE 20
 
-int get_line(char s[], int limit);
-void copy(char to[], char from[]);
 
 int main() {
-    int max;
-    int len;
-    char line[MAXLINE];
-    char longest[MAXLINE];
-    max = 0;
-    while((len = get_line(line, MAXLINE)) > 0) {
-       if(len > max) {
-            max = len;
-            copy(longest, line);
-       }
-    }
-    if (max > 0) {
-        printf("%s %d\n", longest, max);
-    }
+    int c, blank_num, out_len;
+    out_len = 0;
+    blank_num = 0;
 
-}
-
-
-int get_line(char s[], int limit) {
-    int c, i, j;
-    j = 0;
-    for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i) {
-        if (i < limit -2) {
-            s[j] = c;
-            ++j;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+           blank_num++; 
+        } else if (c != '\n') {
+            printf("%*s", blank_num, "");
+            out_len += blank_num;
+            putchar(c);
+            blank_num = 0;
+            out_len++;
         }
     }
-    if (c == '\n') {
-        s[j] = c;
-        ++j;
-        ++i;
-    }
-    s[j] = '\0';
-    return i;
+    printf("\n");
+    printf("%d\n", out_len);
 }
 
 
-void copy(char to[], char from[]) {
-    int c, i;
-    i = 0;
-    while ((to[i] = from[i]) != '\0') {
-        ++i;
-    }
-}
