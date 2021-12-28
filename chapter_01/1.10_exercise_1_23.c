02df130a8498eca6247196387efb60530ebbb357
index003 <index003@sina.cn>
2021-12-28 15:38:13 +0800

    update 1-23

--- chapter_01/1.10_exercise_1_23.c
+++ chapter_01/1.10_exercise_1_23.c
@@ -1,79 +1,47 @@
 #include <stdio.h>
-# define MAXLINE 100
 
-/* first commit  */
-
-/* 
-
-block commit  
-
-*/
-
-// line commit
-int remove_commit(char s[]);
-int get_line(char s[], int limit);
-void copy(char to[],char from[]);
 
 int main() {
-    int len;
-    char line[MAXLINE];
-    len = 0;
-    while (get_line(line, MAXLINE) > 0) {
-        len = remove_commit(line);
-        for (int i =0; i < len; i++) {
-            putchar(line[i]);
+    int c, slash_count, star_count, slash_flag, star_flag;
+    slash_count = 0;
+    star_count = 0;
+    slash_flag = 0;
+    star_flag = 0;
+    while ((c = getchar()) != EOF && c != '\n' && slash_flag == 0) {
+        if (c == '/') {
+            if (slash_count == 0) {
+                slash_count = 1;
+            } else if (slash_count == 1) {
+                slash_flag = 1;
+            }
+        } else if (c == '*') {
+            if (slash_count == 0) {
+                putchar(c);
+            } else if (slash_count == 1) {
+                while ((c = getchar()) != EOF && c != '\n' && star_flag == 0) {
+                    if (c == '*' && star_count == 0) {
+                        star_count = 1;
+                    } 
+                    if (c == '/') {
+                        if (star_count == 1) {
+                            star_flag = 1;
+                            slash_count = 0;
+                            star_count = 0;
+                        }
+                    }
+                    else if(c == '\n') {
+                        putchar(c);
+                    }
+               }
+               star_flag = 0;
+            }
+        } else if (slash_count == 1) {
+            putchar('/');
+            putchar(c);
+        } else {
+            putchar(c);
         }
     }
-    
+    putchar('\n');
 }
 
-int remove_commit(char s[]) {
-    int i = 0;
-    int len = 0;
-    int flag = 0;
-    int commit_start = 0;
-    int commit_posion = 0;
-    while (s[i] != '\n' && s[i] != '\0') {
-        i++;
-    }
-    for (int j = 0; j < i; j ++) {
-        if (s[j] =='/' && commit_start == 0 && flag == 0) {
-            commit_start = 1;
-        }
-        if (s[i] !='/' && commit_start == 1 && flag == 0) {
-            commit_start = 0;
-        }
-        if (s[j] == '/' && commit_start == 1 && flag ==0) {
-            commit_start = 2;
-            flag = 1;
-            commit_posion = j;
-        }
-        len = commit_posion - 1;
-        return len;
-    }
-
-}
-int get_line(char s[], int limit) {
-    int c, i, j;
-    j = 0;
-    for (i = 0; (c = getchar()) != EOF && c != '\n'; i++) {
-        if (i < limit -2) {
-            s[j] = c;
-            j++;
-        }
-    }
-    if (c == '\n') {
-        s[j] = c;
-        j++;
-        i++;
-    }
-    s[j] = '\0';
-    return i;
-}
-
-void copy(char to[], char from[]) {
-    int i = 0;
-    while ((to[i] = from[i]) != '\0') {
-        i++;
-    }
-}
