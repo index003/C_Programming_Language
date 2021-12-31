42cb28e61b74a25f86ebdc36b01ab1bbb87b4e3b
index003 <index003@sina.cn>
2021-12-31 11:57:22 +0800

    update 1.23

--- chapter_01/1.10_exercise_1_23.c
+++ chapter_01/1.10_exercise_1_23.c
@@ -4,7 +4,7 @@
 #define ONE_SLASH 1     // 第一个斜杠
 #define TWO_SLASH 2     // 第二个斜杠
 #define SLASH_STAR 3    // 斜杠和星号
-
+#define STRING_FLAG 4
 
 
 int main() {
@@ -13,32 +13,40 @@ int main() {
     char lc ='\0'; //记录上一个字符内容
 
     while ((c = getchar()) != EOF) {
-       if (state == NORMAL) {
-            if (c == '/') {
-                state = ONE_SLASH;
-            } else {
-                putchar(c);
+        if (state == NORMAL) {
+             if (c == '/') {
+                 state = ONE_SLASH;
+             } else if (c == '"'){
+                 state = STRING_FLAG;
+                 putchar(c);
+             } else {
+                 putchar(c);
+             }
+        } else if (state == ONE_SLASH) {
+             if (c == '/' && lc == '/') {
+                 state = TWO_SLASH; 
+             } else if (c == '*' && lc == '/') {
+                 state = SLASH_STAR;
+             } else {
+                 putchar('/');
+                 putchar(c);
+                 state = NORMAL;
+             }   
+        } else if (state == TWO_SLASH) {
+             if (c == '\n') {
+                 putchar(c);
+                 state = NORMAL;
+             }
+        } else if (state == SLASH_STAR) {
+             if (c == '/' && lc == '*') {
+                 state = NORMAL;
+             }   
+        } else if (state == STRING_FLAG) {
+            if (c == '"') {
+                state = NORMAL;
             }
-       } else if (state == ONE_SLASH) {
-            if (c == '/' && lc == '/') {
-                state = TWO_SLASH; 
-            } else if (c == '*' && lc == '/') {
-                state = SLASH_STAR;
-            } else {
-                putchar('/');
-                putchar(c);
-                state = NORMAL;
-            }   
-       } else if (state == TWO_SLASH) {
-            if (c == '\n') {
-                putchar(c);
-                state = NORMAL;
-            }
-       } else if (state == SLASH_STAR) {
-            if (c == '/' && lc == '*') {
-                state = NORMAL;
-            }   
-       }
-       lc = c;
+            putchar(c);
+        }
+        lc = c;
     }
 }
