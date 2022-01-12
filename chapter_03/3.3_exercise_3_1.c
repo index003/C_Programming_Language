e1ac3f1546e8e8d968f2e9e12133596310b25282
index003 <index003@sina.cn>
2022-01-12 16:27:34 +0800

    3-1

--- chapter_03/3.3_exercise_3_1.c
+++ chapter_03/3.3_exercise_3_1.c
@@ -18,16 +18,19 @@ int binsearch(int x, int v[], int n) {
 
     low = 0;
     high = n - 1;
-    while (low <= high) {
-        mid = (low + high) / 2;
-        if (x < v[mid]) {
+    mid = (low + high) / 2;
+    while (low <= high && x != v[mid]) {
+        if (x <= v[mid]) {
             high = mid + 1;
-        } else if (x > v[mid]) {
-            low = mid + 1;
         } else {
-            return mid;
+            low = mid + 1;
         }
+        mid = (low + high) / 2;
     }
-    return -1;
+    if (x == v[mid]) {
+        return mid;
+    } else {
+        return -1;
+    }
 
 }
