4d8da160fa39a26b4314eff5f7ced5676f884dec
index003 <index003@sina.cn>
2022-01-12 14:25:22 +0800

    update 2-8

--- chapter_02/2.9_exercise_2_8.c
+++ chapter_02/2.9_exercise_2_8.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
 
 int rightrot(int x, int n);
+int wordlength();
 
 int main() {
-    int x = 0b01110101; // 117
+    int x = 0b01100001; // 117
     printf("%u\n", x); 
     printf("%u\n", rightrot(x, 3)); 
 }
 
 
 int rightrot(int x, int n) {
-    int i, j, k;
-    // 右侧n位置为1，其他位置为0
-    i = ~(~0 << n);
+    int i, j, k, l;
     // 保留x右侧n位的值 
-    j = x & i;
-    // 00000101 5
-    return j;
+    i = x & ~(~0 << n);
+    // 保留x右侧n位的值推到最左侧
+    j = i << wordlength() - n ;
+    // 合并
+    k = (x >> n) | j;
+    return k;
+}
+
+int wordlength(void) {
+    int i ;
+    unsigned x = (unsigned) ~0;
+    for (i = 1;x = x >> 1; i++) {
+        ;
+    }
+    return i;
 }
