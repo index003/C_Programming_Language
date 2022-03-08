b0c932a42402368a2a5c01d74a00195fa350cb24
index003 <index003@sina.cn>
2022-03-08 14:59:08 +0800

    update 5-5std

--- chapter_05/5-5.strn_std.c
+++ chapter_05/5-5.strn_std.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /* p_strncpy: copy n characters from t to s */
 void p_strncpy(char *s, char *t, int n);
@@ -14,50 +15,42 @@ int main() {
     char s[100];
     char *t = "abcde";
     int n = 2;
-    printf("str_ncpy('%s', '%s', %d) => ", s, t, n);
+    printf("p_strncpy('%s', '%s', %d) => ", s, t, n);
     p_strncpy(s, t, n);
     printf("'%s'\n", s);
 
     char s1[100] = "abcde";
     char *t1 = "fghij";
-    printf("str_ncat('%s', '%s', %d) => ", s1, t1, n);
+    printf("p_strncat('%s', '%s', %d) => ", s1, t1, n);
     p_strncat(s1, t1, n);
     printf("'%s'\n", s1);
 
     char *s2 = "abcde";
     char *t2 = "abfe";
-    printf("str_ncmp('%s', '%s', %d) => %d\n", s2, t2, n, p_strncmp(s2, t2, n));
+    printf("p_strncmp('%s', '%s', %d) => %d\n", s2, t2, n, p_strncmp(s2, t2, n));
     n = 3;
-    printf("str_ncmp('%s', '%s', %d) => %d\n", s2, t2, n, p_strncmp(s2, t2, n));
+    printf("p_strncmp('%s', '%s', %d) => %d\n", s2, t2, n, p_strncmp(s2, t2, n));
 
     return 0;
 }
 
 void p_strncpy(char *s, char *t, int n) {
-    while (*s){
-        s++;
-    }
-    while (n > 0 && (*s++ = *t++)) {
-        n--;
+    while (*t && n-- > 0){
+        *s++ = *t++;
     }
     *s = '\0';
+    
 }
 
 void p_strncat(char *s, char *t, int n) {
-    while (*s){
-        s++;
-    }
-    while (n > 0 && (*s++ = *t++)) {
-        n--;
-    }
-    *s = '\0';
+    void p_strncpy(char *s, char *t, int n);
+    int slen = strlen(s);
+    strncpy(s + slen, t, n);
 }
 
 int p_strncmp(char *s, char *t, int n) {
-    //for (; *s == *t; s++,t++) {
-    while (n > 0 && (*s++ == *t++)) {
-        n--;
-        if (*s == '\0') {
+    for (; *s == *t; s++,t++) {
+        if (*s == '\0' || --n <= 0) {
             return 0;
         }
     }
